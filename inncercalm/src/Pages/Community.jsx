8bf20f5c0026f3800676254cfd3cde8739fd3aa4
iamhk12 <hkothari247@gmail.com>
2023-04-09 17:34:48 +0530

    hk

--- inncercalm/src/Pages/Community.jsx
+++ inncercalm/src/Pages/Community.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Nav from '../components/Nav_nothome'
 import defpp from '../components/defpp.jpg'
 import "./Community.css"
@@ -6,16 +6,88 @@ import "./Community.css"
 const Community = () => {
 
     const [showForm, setShowForm] = useState(false);
-
+    const [suberr, setSubErr] = useState(false);
+    const [name, setName] = useState('');
+    const [story, setStory] = useState('');
     const handleShareClick = () => {
         setShowForm(true);
     };
 
-    const handleCloseForm = () => {
-        setShowForm(false);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch('http://localhost:5000/newpost', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name, story }),
+        })
+            .then((response) => {
+                response.json()
+                console.log(response)
+                if (response.status === 200) {
+                    setName("")
+                    setStory("")
+                    handleCloseForm();
+                    FetchPosts()
+
+                } else {
+
+                    setSubErr(true)
+                }
+            })
+            .then((data) => {
+                console.log(data);
+                // do something with the response data
+
+
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                setSubErr(true)
+
+            });
+
     };
 
 
+    const handleCloseForm = () => {
+        setName("")
+        setStory("")
+        setShowForm(false);
+        setSubErr(false)
+    };
+
+
+    const [allposts, setAllPosts] = useState([])
+
+    const FetchPosts = async () => {
+        try {
+            const resFromBack = await fetch('http://localhost:5000/allposts', {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                }
+            })
+
+            const data = await resFromBack.json()
+            setAllPosts(data)
+            console.log(data)
+
+            if (resFromBack.status !== 200 || !data) {
+                console.log("Not logged in")
+                window.alert("Error")
+            }
+
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        FetchPosts()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     return (
         <>
@@ -25,11 +97,13 @@ const Community = () => {
                 <header>We believe that by sharing our experiences, we can help others feel less alone and inspire them to seek the help they need. Join us in creating a supportive and inclusive space where everyone's voice is heard.</header>
 
                 <h1 className="head_st">
-                    Inspiring Stories of People
+                    Stories of People
                 </h1>
 
                 <div className="allposts">
-                    <div className="post">
+
+                    {/* ////////////////// */}
+                    {/* <div className="post">
                         <div className="posthead">
                             <img src={defpp} alt="" />
                             <p className="name">Anonymous</p>
@@ -41,47 +115,31 @@ const Community = () => {
                             </p>
 
                         </div>
-                    </div>
-                    <div className="post">
-                        <div className="posthead">
-                            <img src={defpp} alt="" />
-                            <p className="name">Anonymous</p>
-                        </div>
+                    </div> */}
 
-                        <div className="post_content">
-                            <p className="post_text">
-                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque vero officia quidem aspernatur fugiat eaque quis fuga maiores laudantium expedita quisquam sed nemo itaque unde, quae id sequi facere pariatur facilis eum optio aut accusamus. Ipsam consequuntur voluptate nobis, vel facilis fuga sint praesentium, natus nulla vitae repudiandae nostrum aut distinctio perferendis nisi tempora necessitatibus magnam delectus, modi adipisci voluptates quam? Asperiores corrupti, incidunt perferendis magnam inventore cupiditate labore cumque provident consequuntur, earum repellat ut quas quod hic.
-                            </p>
+                    {allposts.map((val) => {
+                        return <>
+                            <div className="post">
+                                <div className="posthead">
+                                    <img src={defpp} alt="" />
+                                    <p className="name">{"User" + Math.floor(Math.random() * 1000000)}</p>
+                                </div>
 
-                        </div>
-                    </div>
-                    <div className="post">
-                        <div className="posthead">
-                            <img src={defpp} alt="" />
-                            <p className="name">Anonymous</p>
-                        </div>
+                                <div className="post_content">
+                                    <p className="post_text">
+                                        {val.story}
+                                    </p>
 
-                        <div className="post_content">
-                            <p className="post_text">
-                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque vero officia quidem aspernatur fugiat eaque quis fuga maiores laudantium expedita quisquam sed nemo itaque unde, quae id sequi facere pariatur facilis eum optio aut accusamus. Ipsam consequuntur voluptate nobis, vel facilis fuga sint praesentium, natus nulla vitae repudiandae nostrum aut distinctio perferendis nisi tempora necessitatibus magnam delectus, modi adipisci voluptates quam? Asperiores corrupti, incidunt perferendis magnam inventore cupiditate labore cumque provident consequuntur, earum repellat ut quas quod hic.
-                            </p>
+                                </div>
+                            </div>
+                        </>
+                    })}
+                    {/* ////////////////// */}
 
-                        </div>
-                    </div>
-                    <div className="post">
-                        <div className="posthead">
-                            <img src={defpp} alt="" />
-                            <p className="name">Anonymous</p>
-                        </div>
 
-                        <div className="post_content">
-                            <p className="post_text">
-                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque vero officia quidem aspernatur fugiat eaque quis fuga maiores laudantium expedita squisquam sed nemo itaque unde, quae id sequi facere pariatur facilis eum optio aut accusamus. Ipsam consequuntur voluptate nobis, vel facilis fuga sint praesentium, natus nulla vitae repudiandae nostrum aut distinctio perferendis nisi tempora necessitatibus magnam delectus, modi adipisci voluptates quam? Asperiores corrupti, incidunt perferendis magnam inventore cupiditate labore cumque provident consequuntur, earum repellat ut quas quod hic.
-                            </p>
-
-                        </div>
-                    </div>
                 </div>
+
+
 
                 <div className="sharesection">
                     <button className="share_btn" onClick={handleShareClick}>
@@ -92,15 +150,27 @@ const Community = () => {
 
                 {showForm && (
                     <div className="form-popup">
-                        <form>
+                        <form onSubmit={handleSubmit}>
+
                             <h3>Share Your Story</h3>
-                            <label htmlFor="name">Name:</label>
-                            <input type="text" name="name" />
+                            <label htmlFor="name">Name: <span>(Your name will be hidden)</span></label>
+                            <input
+                                type="text"
+                                name="name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
                             <label htmlFor="story">Your Story:</label>
-                            <textarea name="story" />
+                            <textarea
+                                rows={10}
+                                name="story"
+                                value={story}
+                                onChange={(e) => setStory(e.target.value)}
+                            />
+                            {suberr ? <span className="errormessage"> *Error Posting the story</span> : <></>}
                             <div className="form-buttons">
-                                <button type="submit">Submit</button>
-                                <button type="button" onClick={handleCloseForm}>
+                                <button type="submit" className="submitbtn" >Submit</button>
+                                <button className="closebtn" type="button" onClick={handleCloseForm}>
                                     Close
                                 </button>
                             </div>
