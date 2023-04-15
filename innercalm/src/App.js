223058af806594d552d42654799e398ce77bfd42
AbduRahim <77384491+BlazeRahim@users.noreply.github.com>
2023-04-15 22:21:33 +0530

    app js changes

--- innercalm/src/App.js
+++ innercalm/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom'
-// import Nav from './components/Nav';
 import './App.css';
 import Home from './Pages/Home';
 import Community from './Pages/Community';
 import Education from './Pages/Education';
+import Music from './Pages/Music';
+import Contact from './Pages/Contact';
+import Helplines from './Pages/Helplines';
 
 
 function App() {
@@ -16,6 +18,9 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/community' element={<Community />} />
         <Route path='/education' element={<Education />} />
+        <Route path='/music' element={<Music />} />
+        <Route path='/helplines' element={<Helplines />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </>
   );
