07ed4ef1362f07865c8eabd17019bad0913fdaf2
iamhk12 <hkothari247@gmail.com>
2023-04-16 11:13:26 +0530

    hk

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom'
+// import Nav from './components/Nav';
 import './App.css';
 import Home from './Pages/Home';
 import Community from './Pages/Community';
